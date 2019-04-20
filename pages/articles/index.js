d9ade648080424422e342330ee9194028601423c
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-20 11:23:07 +0800

    Support refresh in ArticlesIndexPage

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import factory from '../../ethereum/instances/factory';
 import Layout from '../../client/components/Layout';
 import { Link } from '../../routes';
@@ -10,14 +11,32 @@ export default class ArticlesIndexPage extends Component {
     return { articles };
   }
 
+  state = {
+    refreshing: false,
+    articles: this.props.articles,
+  };
+
+  refresh = async () => {
+    this.setState({ articles: [], refreshing: true });
+    const articles = await factory.methods.getArticles().call();
+    this.setState({ articles, refreshing: false });
+  };
+
   render() {
+    const { articles, refreshing } = this.state;
+
     return (
       <Layout>
-        <h3>Published articles</h3>
-        <div style={{ marginBottom: 10 }}>
-          <Link route='/articles/new'><a className='ui button color orange'>Draft article</a></Link>
+        <div style={{ marginBottom: 10, display: 'flex', justifyContent: 'space-between' }}>
+          <h1>Published articles</h1>
+          <div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Link route='/articles/new'><a style={{ marginRight: 10 }} className='ui button color orange'>Draft article</a></Link>
+              <Button basic secondary content='Refresh' icon='refresh' onClick={this.refresh} disabled={refreshing} loading={refreshing} />
+            </div>
+          </div>
         </div>
-        <ArticleList articles={this.props.articles} getLink={addr => `/articles/${addr}`} />
+        <ArticleList articles={articles} getLink={addr => `/articles/${addr}`} />
       </Layout>
     );
   }
