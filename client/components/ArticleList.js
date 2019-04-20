e9efe381f7d122974c9343621ea1b2022809de43
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-20 20:30:53 +0800

    Add pagination in ArticleList

--- client/components/ArticleList.js
+++ client/components/ArticleList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Loader } from 'semantic-ui-react';
+import { Card, Loader, Pagination } from 'semantic-ui-react';
 import h2p from 'html2plaintext';
 import { Link } from '../../routes';
 import loadArticleDetail from '../../client/utils/loadArticleDetail';
@@ -13,7 +13,9 @@ export default class ArticleList extends Component {
   };
 
   state = {
-    articles: {},
+    articlesDetail: {},
+    itemsPerPage: 10,
+    activePage: 1, // start from 1
   };
 
   load = (articles) => {
@@ -21,9 +23,9 @@ export default class ArticleList extends Component {
       if (!cachedArticles[addr])
         loadArticleDetail(addr).then(detail => {
           this.setState(state => {
-            state.articles[addr] = detail;
+            state.articlesDetail[addr] = detail;
             cachedArticles[addr] = detail;
-            return { articles: { ...state.articles } };
+            return { articlesDetail: { ...state.articlesDetail } };
           });
         });
     });
@@ -40,28 +42,45 @@ export default class ArticleList extends Component {
   }
 
   render() {
-    const items = this.props.articles.map(addr => {
-      const article = this.state.articles[addr] || cachedArticles[addr];
+    const { articles: addresses } = this.props;
+    const { articlesDetail, activePage, itemsPerPage } = this.state;
 
-      if (!article) {
+    const items = addresses
+      .filter((_, index) => {
+        return index >= (activePage - 1) * itemsPerPage && index < activePage * itemsPerPage;
+      })
+      .map(addr => {
+        const article = articlesDetail[addr] || cachedArticles[addr];
+
+        if (!article) {
+          return {
+            key: addr,
+            header: <span style={{ fontFamily: 'monospace', whiteSpace: 'nowrap' }}>{addr}</span>,
+            meta: 'Loading...',
+            description: <Loader active />,
+            fluid: true,
+          };
+        }
+
         return {
-          key: addr,
-          header: <span style={{ fontFamily: 'monospace', whiteSpace: 'nowrap' }}>{addr}</span>,
-          meta: 'Loading...',
-          description: <Loader active />,
+          header: article.title,
+          meta: <Link route={this.props.getLink(addr)}><a>View article</a></Link>,
+          description: <p style={styles.bodyAbstract}>{h2p(article.body)}</p>,
           fluid: true,
         };
-      }
+      });
 
-      return {
-        header: article.title,
-        meta: <Link route={this.props.getLink(addr)}><a>View article</a></Link>,
-        description: <p style={styles.bodyAbstract}>{h2p(article.body)}</p>,
-        fluid: true,
-      };
-    });
-
-    return <Card.Group items={items} />;
+    return (
+      <div>
+        <Card.Group items={items} />
+        <Pagination
+          style={{ marginTop: 20 }}
+          activePage={activePage}
+          onPageChange={(e, { activePage }) => this.setState({ activePage })}
+          totalPages={Math.ceil(addresses.length / itemsPerPage)}
+        />
+      </div>
+    );
   }
 }
 
